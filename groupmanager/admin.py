bc264b891fb2afd1b10a096519dd7b096a5927f7
egeu5 <christoph@ohrfrei.eu>
2022-02-15 21:45:22 +0100

    Import der Datenstruktur

--- groupmanager/admin.py
+++ groupmanager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+admin.site.register(Question)
+
+from .models import Group
+admin.site.register(Group)
+
+from .models import Participant
+admin.site.register(Participant)
