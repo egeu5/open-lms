d1f88c6f61cc09287f63a9a8e046db3a3e06d686
Christoph Richter <christoph@ohrfrei.eu>
2022-02-15 11:16:39 +0100

    dumme kommentare gelöscht

--- src/personen/models.py
+++ src/personen/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class Question(models.Model):
     assignment = models.CharField(max_length=50)
     assignment2 = models.CharField(max_length=50)
-    # yeeeeeet
-    # yeeeeeeeeeeet
+    
     
 
 
