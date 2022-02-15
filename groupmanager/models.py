aafb6a1690fc0e22ab003ca18a462e4e83a0d59d
egeu5 <christoph@ohrfrei.eu>
2022-02-15 21:44:31 +0100

    Datenstruktur angelegt

--- groupmanager/models.py
+++ groupmanager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#1 Question contains N Groups
+class Question(models.Model):
+    name = models.CharField(max_length=50)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+
+#N Participant answear 1 Question
+class Participant(models.Model):
+    name = models.CharField(max_length=50)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
