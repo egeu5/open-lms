735b94e851ea786f1febc522e688e5308f9f9577
saproko <saschalibricht@gmx.de>
2022-02-17 12:58:38 +0100

    Registrierung des models verschönern

--- groupmanager/admin.py
+++ groupmanager/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
-from .models import Event
+# registriere models
 admin.site.register(Event)
-
-from .models import Topic
 admin.site.register(Topic)
-
-from .models import Participant
 admin.site.register(Participant)
